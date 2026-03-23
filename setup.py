4d0f60f2142d755cc5045d82567e6461cc03fda0
rsscml <rahul.sinhaocp@gmail.com>
2026-03-23 09:07:00 +0530

    update versions in requirements

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="tft_pytorch",
-    version="0.1.0",
+    version="0.1.1",
     author="",
     description="PyTorch implementation of the Temporal Fusion Transformer (TFT) for time-series forecasting",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=[
-        "torch>=1.13",
+        "torch>=2.5",
         "numpy>=1.21",
-        "pandas>=1.3",
-        "scikit-learn>=1.0",
+        "pandas>=2.2",
+        "scikit-learn>=1.1",
         "joblib>=1.1",
     ],
     extras_require={
